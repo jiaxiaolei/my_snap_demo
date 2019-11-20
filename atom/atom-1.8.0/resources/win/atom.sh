0c0ea905cfd7e574e5ab9662561e699282bed0ec
jiaxiaolei <jiaxiaolei@cicaero.com>
2019-11-20 10:34:45 +0800

    --delete big file

--- atom/atom-1.8.0/resources/win/atom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if command -v "cygpath" > /dev/null; then
-  ATOMCMD=""$(cygpath "$(dirname "$0")" -a -w)\\atom.cmd""
-else
-  pushd "$(dirname "$0")" > /dev/null
-  ATOMCMD=""$(pwd -W)/atom.cmd""
-  popd > /dev/null
-fi
-cmd.exe /C "$ATOMCMD" "$@"
